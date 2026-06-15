c3e1b29e19ef97b7d49c2aae75f3ff53630566e6
mustafaafyalensAI <mustafapandu99@gmail.com>
2026-06-15 05:28:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import os
 
+app = Flask(__name__)
+
+model = tf.keras.models.load_model("malaria_final_model_v2.keras")
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files["image"]
+
+    path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(path)
+
+    img = image.load_img(path, target_size=(128,128))
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)[0][0]
+
+    if prediction < 0.5:
+        result = "PARASITIZED"
+        confidence = float((1 - prediction) * 100)
+    else:
+        result = "UNINFECTED"
+        confidence = float(prediction * 100)
+
+    return jsonify({
+        "result": result,
+        "confidence": confidence
+    })
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
